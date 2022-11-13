1d6353c83306d3b83b0c9816d07b3227248cd760
Uriel Hernandez <urielHernandezgarcia1@gmail.com>
2022-11-13 15:29:26 -0600

    commit5

--- src/index.js
+++ src/index.js
@@ -91,8 +91,20 @@ function isUserSignedIn() {
 
 // Saves a new message on the Cloud Firestore.
 async function saveMessage(messageText) {
-  // TODO 7: Push a new message to Cloud Firestore.
+  // Add a new message entry to the Firebase database.
+  try {
+    await addDoc(collection(getFirestore(), 'messages'), {
+      name: getUserName(),
+      text: messageText,
+      profilePicUrl: getProfilePicUrl(),
+      timestamp: serverTimestamp()
+    });
+  }
+  catch(error) {
+    console.error('Error writing new message to Firebase Database', error);
+  }
 }
+  // TODO 7: Push a new message to Cloud Firestore.
 
 // Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
